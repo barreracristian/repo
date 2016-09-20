cd723719f7d1abc5e2ffc7f13cda8726207ff573
Cristian Barrera <cbarrera@nubato.com>
2016-09-20 00:13:44 -0300

    cart++

--- app/js/controllers/CartController.js
+++ app/js/controllers/CartController.js
@@ -4,31 +4,79 @@ angular.module('repo.controllers.CartController', [])
 
             $scope.cart = CartService.getCart();
 
-            _.each(UtilService.getFeaturedProducts(4), function(prod){
+            _.each(UtilService.getFeaturedProducts(4), function (prod) {
                 $scope.cart.addProduct(prod, 1 + parseInt(Math.random() * 10));
             });
 
-            $scope.checkoutStep = 'resumen';
-            $scope.data = {};
+            $scope.talleres = [
+                {
+                    id: 1,
+                    street: 'Diez de Julio',
+                    number: '354',
+                    detail: 'esquina Sta. Rosa',
+                    commune: 'Santiago'
+                },
+                {
+                    id: 2,
+                    street: 'Av Matta',
+                    number: '7734',
+                    detail: 'Local B1',
+                    commune: 'Maipu'
+                }
+            ];
 
-            $scope.removeFromCart = function(prod){
-                CartService.getCart().removeProduct(prod);
+            $scope.getTaller = function(id){
+                return _.find($scope.talleres, {id: parseInt(id)});
+            };
+
+
+
+            $scope.removeFromCart = function (prod) {
+                if($scope.checkoutStep == 'delivery' || $scope.checkoutStep == 'payment'){
+                    CartService.getCart().removeProduct(prod);
+                }
             };
 
             //Chekout Steps
 
-            $scope.selectDelivery = function(what){
-                $scope.data.delivery = what;
+            $scope.checkoutStep = 'delivery'; //delivery, payment, resumen, finished
+            $scope.data = {};
+
+            $scope.changeCheckoutStep = function(to){
+                if($scope.goodToGoTo(to)){
+                    $scope.checkoutStep = to;
+                    if(to == 'finished'){
+                        CartService.reset();
+                    }
+                }
             };
 
-            $scope.selectPayment = function(what){
-                $scope.data.payment = what;
-            };
+            $scope.goodToGoTo = function (what) {
+                //payment
+                if (what == 'payment') {
+                    if ($scope.data.deliveryType == 'taller' && $scope.data.selectedTallerId) {
+                        return true;
+                    }
+                    if ($scope.data.deliveryType == 'domicilio' &&
+                        $scope.data.address &&
+                        $scope.data.commune) {
+                        return true;
+                    }
+                    return false;
+                } else if(what == 'resumen'){
+                    if ($scope.data.paymentType == 'transferencia') {
+                        return true;
+                    }
+                    if ($scope.data.paymentType == 'taller') {
+                        return true;
+                    }
+                    return false;
+                } else if(what == 'finished'){
+                    return true;
+                }
 
-            $scope.finish = function () {
-                $scope.checkoutStep = 'finished';
-                CartService.reset();
-            };
+                return false;
+            }
 
         }
     )
